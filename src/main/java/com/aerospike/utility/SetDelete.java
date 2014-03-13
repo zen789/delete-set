59e5c67bbd44725dfec3cdbb5b421f5ba88f128c
peter <peter@aerospike.com>
2014-03-13 16:32:31 -0500

    Cleaner error handeling

--- src/main/java/com/aerospike/utility/SetDelete.java
+++ src/main/java/com/aerospike/utility/SetDelete.java
@@ -15,6 +15,7 @@ import com.aerospike.client.AerospikeClient;
 import com.aerospike.client.AerospikeException;
 import com.aerospike.client.Key;
 import com.aerospike.client.Record;
+import com.aerospike.client.ResultCode;
 import com.aerospike.client.ScanCallback;
 import com.aerospike.client.policy.ScanPolicy;
 import com.aerospike.client.policy.WritePolicy;
@@ -22,7 +23,7 @@ import com.aerospike.client.policy.WritePolicy;
 public class SetDelete {
 	private static Logger log = Logger.getLogger(SetDelete.class);
 	static int count = 0;
-	public static void main(String[] args) throws ParseException, AerospikeException {
+	public static void main(String[] args) throws ParseException {
 		Options options = new Options();
 		options.addOption("h", "host", true, "Server hostname (default: localhost)");
 		options.addOption("p", "port", true, "Server port (default: 3000)");
@@ -32,13 +33,13 @@ public class SetDelete {
 
 		CommandLineParser parser = new PosixParser();
 		CommandLine cl = parser.parse(options, args, false);
-		
+
 		if (args.length == 0 || cl.hasOption("u")) {
 			logUsage(options);
 			return;
 		}
 
-		
+
 		String host = cl.getOptionValue("h", "127.0.0.1");
 		String portString = cl.getOptionValue("p", "3000");
 		int port = Integer.parseInt(portString);
@@ -49,34 +50,40 @@ public class SetDelete {
 		log.info("Port: " + port);
 		log.info("Name space: " + namespace);
 		log.info("Set: " + set);
-		
-		final AerospikeClient client = new AerospikeClient(host, port);
-		
-		
-		ScanPolicy scanPolicy = new ScanPolicy();
-		/*
-		 * scan the entire Set using scannAll(). This will scan each node 
-		 * in the cluster and return the record Digest to the call back object
-		 */
-		client.scanAll(scanPolicy, namespace, set, new ScanCallback() {
-			
-			public void scanCallback(Key key, Record record) throws AerospikeException {
-				/*
-				 * for each Digest returned, delete it using delete()
-				 */
-				client.delete(new WritePolicy(), key);
-				count++;
-				/*
-				 * after 25,000 records delete, return print the count.
-				 */
-				if (count % 25000 == 0){
-					log.info("Deleted "+ count);
+		try {
+			final AerospikeClient client = new AerospikeClient(host, port);
+
+
+			ScanPolicy scanPolicy = new ScanPolicy();
+			/*
+			 * scan the entire Set using scannAll(). This will scan each node 
+			 * in the cluster and return the record Digest to the call back object
+			 */
+			client.scanAll(scanPolicy, namespace, set, new ScanCallback() {
+
+				public void scanCallback(Key key, Record record) throws AerospikeException {
+					/*
+					 * for each Digest returned, delete it using delete()
+					 */
+					client.delete(new WritePolicy(), key);
+					count++;
+					/*
+					 * after 25,000 records delete, return print the count.
+					 */
+					if (count % 25000 == 0){
+						log.info("Deleted "+ count);
+					}
 				}
-			}
-		}, new String[] {});
-		log.info("Deleted "+ count + " records from set " + set);
+			}, new String[] {});
+			log.info("Deleted "+ count + " records from set " + set);
+		} catch (AerospikeException e) {
+			int resultCode = e.getResultCode();
+			log.info(ResultCode.getResultString(resultCode));
+			log.debug("Error details: ", e);
+		};
 	}
-	
+
+
 	private static void logUsage(Options options) {
 		HelpFormatter formatter = new HelpFormatter();
 		StringWriter sw = new StringWriter();
